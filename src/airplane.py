230172f484dba2eb075e8f0df4f1e9b324df96bb
Vadim Semenov <vadimsemenov53@gmail.com>
2026-03-18 19:38:21 +0300

    add class Airplane

---
+++ src/airplane.py
@@ -0,0 +1,58 @@
+class Airplane:
+    """ Класс отдельного самолета с атрибутами и методами валидации. """
+    __slots__ = ("country", "callsign", "velocity", "vertical_rate", "bar_altitude")
+
+    def __init__(self, country: str, callsign: str, velocity: float, vertical_rate: float, bar_altitude: float):
+        """Метод - конструктор, для инициализации объектов класса."""
+        self.country = country
+        self.callsign = callsign
+        self.velocity = velocity
+        self.vertical_rate = vertical_rate
+        self.bar_altitude = bar_altitude
+
+
+    @staticmethod
+    def _validate_velocity(velocity: float) -> float:
+        """ Приватный метод валидации данных (скорость). """
+        if velocity is None or not isinstance(velocity, int | float):
+            return 0
+        return velocity
+
+
+    @staticmethod
+    def _validate_bar_altitude(bar_altitude: float) -> float:
+        """ Приватный метод валидации данных (скорость). """
+        if bar_altitude is None or not isinstance(bar_altitude, int | float):
+            return 0
+        return bar_altitude
+
+
+    def __repr__(self):
+        """ Метод отладки, для разработчика """
+        return (f'Airplane: {self.callsign}, {self.country}'
+                f'Velocity: {self.velocity}, Bar_A: {self.bar_altitude}')
+
+
+    def __lt__(self, other: 'Airplane') -> bool:
+        """ Метод сравнения высот двух самолетов (Air_1 < Air_2). """
+        return self.bar_altitude < other.bar_altitude
+
+    def __gt__(self, other: 'Airplane') -> bool:
+        """ Метод сравнения высот двух самолетов (Air_1 > Air_2). """
+        return self.bar_altitude > other.bar_altitude
+
+    def __le__(self, other: 'Airplane') -> bool:
+        """ Метод сравнения высот двух самолетов (Air_1 <= Air_2). """
+        return self.bar_altitude <= other.bar_altitude
+
+    def __ge__(self, other: 'Airplane') -> bool:
+        """ Метод сравнения высот двух самолетов (Air_1 >= Air_2). """
+        return self.bar_altitude >= other.bar_altitude
+
+    def __eq__(self, other: 'Airplane') -> bool:
+        """ Метод сравнения высот двух самолетов (Air_1 == Air_2). """
+        return self.bar_altitude == other.bar_altitude
+
+    def __ne__(self, other: "Airplane") -> bool:
+        """ Метод сравнения высот двух самолетов (Air_1 != Air_2). """
+        return self.bar_altitude != other.bar_altitude
