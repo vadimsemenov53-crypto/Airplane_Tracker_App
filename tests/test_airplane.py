3c66cc0363d8d8474566a61bd7c2f6536496e47f
Vadim Semenov <vadimsemenov53@gmail.com>
2026-03-20 21:15:19 +0300

    testing metod Aitplane get_dict

--- tests/test_airplane.py
+++ tests/test_airplane.py
@@ -42,3 +42,13 @@ def test_airplane_magic_error(airplane_1):
 
     assert airplane_1.__ne__("222") == NotImplemented
     assert airplane_1.__ne__({"Bar": 2222}) == NotImplemented
+
+
+def test_airplane_get_dict(airplane_1):
+    assert airplane_1.get_dict_from_airplane() == {
+        "country": "Germany",
+        "callsign": "ECA4RT",
+        "velocity": 222.98,
+        "vertical_rate": 0.33,
+        "bar_altitude": 13716,
+    }
