1ac150c1e11c51ed3566cc590fca2c530045edaa
Vadim Semenov <vadimsemenov53@gmail.com>
2026-03-26 13:22:46 +0300

    add new func get_default_path_save

--- src/utils.py
+++ src/utils.py
@@ -10,3 +10,9 @@ def airplanes_to_dicts(airplanes: list[Airplane]) -> list[dict]:
 def ensure_directory(path: str) -> None:
     """ Вспомогательная функция, создает директорию для файла если она еще не существует. """
     os.makedirs(os.path.dirname(path), exist_ok=True)
+
+
+def get_default_path_save(filename: str) -> str:
+    """ Вспомогательная функция, возвращает путь к файлу по умолчанию. """
+    base_dir = os.path.dirname(os.path.dirname(__file__))
+    return os.path.join(base_dir, 'data', filename)
