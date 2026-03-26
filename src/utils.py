a3bb4db946febf2c0abc5fbf77a3e9dc7e84c989
Vadim Semenov <vadimsemenov53@gmail.com>
2026-03-26 13:13:46 +0300

    add new func ensure_directory

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,12 @@
+import os
+
 from src.airplane import Airplane
 
 def airplanes_to_dicts(airplanes: list[Airplane]) -> list[dict]:
     """ Вспомогательная функция для преобразования объектов Airplane в список словарей. """
     return [air.get_dict_from_airplane() for air in airplanes]
+
+
+def ensure_directory(path: str) -> None:
+    """ Вспомогательная функция, создает директорию для файла если она еще не существует. """
+    os.makedirs(os.path.dirname(path), exist_ok=True)
