0ce91e0daa75744f5e15ddb4c037e208e16ad94f
Vadim Semenov <vadimsemenov53@gmail.com>
2026-03-27 14:48:50 +0300

    clean code full project lint black isort mypy flake8

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,14 @@
 from src.utils import airplanes_to_dicts, ensure_directory, get_default_path_save
-from unittest.mock import patch
+
 
 def test_airplanes_to_dicts(airplane_3):
     result = airplanes_to_dicts([airplane_3])
 
-    assert result[0]['country'] == 'Turkey'
-    assert result[0]['callsign'] == 'LVL2517'
-    assert result[0]['velocity'] == 809.77
-    assert result[0]['vertical_rate'] == 0
-    assert result[0]['bar_altitude'] == 11582.4
+    assert result[0]["country"] == "Turkey"
+    assert result[0]["callsign"] == "LVL2517"
+    assert result[0]["velocity"] == 809.77
+    assert result[0]["vertical_rate"] == 0
+    assert result[0]["bar_altitude"] == 11582.4
 
 
 def test_ensure_directory(tmp_path):
@@ -19,6 +19,6 @@ def test_ensure_directory(tmp_path):
 
 
 def test_get_default_path_save():
-    result = get_default_path_save('report.json')
+    result = get_default_path_save("report.json")
 
     assert "Airplane_Tracker_App/data/report.json" in result
