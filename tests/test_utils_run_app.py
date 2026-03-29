be29c4579e675d0d099049865a59fe78463dc40c
Vadim Semenov <vadimsemenov53@gmail.com>
2026-03-29 16:41:48 +0300

    clean code test utils run app

--- tests/test_utils_run_app.py
+++ tests/test_utils_run_app.py
@@ -1,12 +1,10 @@
-import pytest
+from unittest.mock import MagicMock, patch
 
-from unittest.mock import patch, MagicMock
+from interface.utils_run_app import (ask_user, get_airplane_object, get_file_format_user, get_file_manager,
+                                     handle_file_operations, handle_save, show_message, show_report)
 
-from interface.utils_run_app import ask_user, show_message, show_report
-from interface.utils_run_app import get_file_format_user, get_file_manager, get_airplane_object
-from interface.utils_run_app import handle_save, handle_file_operations
 
-@patch('builtins.input', return_value='test input')
+@patch("builtins.input", return_value="test input")
 def test_ask_user(mock_input):
     result = ask_user()
 
@@ -19,7 +17,7 @@ def test_show_message(capsys):
 
     message = capsys.readouterr()
 
-    assert message.out.strip() == 'Программа:\ntest'
+    assert message.out.strip() == "Программа:\ntest"
 
 
 def test_show_report(capsys, airplane_3, airplane_2):
@@ -27,10 +25,10 @@ def test_show_report(capsys, airplane_3, airplane_2):
 
     message = capsys.readouterr()
 
-    assert message.out.strip() == '1. Позывной: LVL2604 | Скорость: 309.77 | Высота: 11582.4'
+    assert message.out.strip() == "1. Позывной: LVL2604 | Скорость: 309.77 | Высота: 11582.4"
 
 
-@patch('builtins.input', return_value=1)
+@patch("builtins.input", return_value=1)
 def test_get_file_format_base(mock_input):
     result = get_file_format_user()
 
@@ -38,25 +36,25 @@ def test_get_file_format_base(mock_input):
     mock_input.assert_called_once_with("Пользователь: ")
 
 
-@patch('builtins.input', return_value=4)
+@patch("builtins.input", return_value=4)
 def test_get_file_format_exit(mock_input, capsys):
     result = get_file_format_user()
     message = capsys.readouterr()
 
     assert not result
-    assert message.out.strip().split('\n')[-1] == 'Завершение работы с файлами.'
+    assert message.out.strip().split("\n")[-1] == "Завершение работы с файлами."
     mock_input.assert_called_once_with("Пользователь: ")
 
 
-@patch('builtins.input', side_effect=[5, 4])
+@patch("builtins.input", side_effect=[5, 4])
 def test_get_file_format(mock_input, capsys):
     result = get_file_format_user()
 
     message = capsys.readouterr()
 
-    assert result == None
-    assert message.out.strip().split('\n')[-1] == 'Завершение работы с файлами.'
-    assert 'Неверный выбор (1, 2, 3, 4)' in message.out.strip().split('\n')
+    assert not result
+    assert message.out.strip().split("\n")[-1] == "Завершение работы с файлами."
+    assert "Неверный выбор (1, 2, 3, 4)" in message.out.strip().split("\n")
 
     mock_input.assert_called_with("Пользователь: ")
 
@@ -109,17 +107,17 @@ def test_get_file_manager_base_6(mock_manager):
     mock_manager.assert_called_once()
 
 
-@patch('builtins.input', return_value="Germany, DLH777, 777, 7, 10000")
+@patch("builtins.input", return_value="Germany, DLH777, 777, 7, 10000")
 def test_get_airplane_object(mock_input):
     result = get_airplane_object()
 
-    assert result.country == 'Germany'
+    assert result.country == "Germany"
     assert result.velocity == 777
 
     mock_input.assert_called_once_with("Пользователь: ")
 
 
-@patch('builtins.input', return_value="Germany, DLH777, AAA, 7, 10000")
+@patch("builtins.input", return_value="Germany, DLH777, AAA, 7, 10000")
 def test_get_airplane_object_invalid_element(mock_input, capsys):
     result = get_airplane_object()
 
@@ -132,7 +130,7 @@ def test_get_airplane_object_invalid_element(mock_input, capsys):
 
 
 @patch("interface.utils_run_app.get_file_manager")
-@patch('builtins.input', side_effect=["file", "/tmp"])
+@patch("builtins.input", side_effect=["file", "/tmp"])
 def test_handle_save_with_filename(mock_input, mock_get_file_manager):
     mock_file = MagicMock()
     mock_get_file_manager.return_value = mock_file
@@ -146,21 +144,12 @@ def test_handle_save_with_filename(mock_input, mock_get_file_manager):
 
 
 @patch("interface.utils_run_app.get_file_manager")
-@patch('builtins.input', side_effect=["1", "/path", "4"])
+@patch("builtins.input", side_effect=["1", "/path", "4"])
 def test_handle_file_read(mock_input, mock_get_file_manager):
     mock_file = MagicMock()
     mock_get_file_manager.return_value = mock_file
-    mock_file.read_file.return_value = ['data']
+    mock_file.read_file.return_value = ["data"]
 
     handle_file_operations()
 
     mock_file.read_file.assert_called_once_with("/path")
-
-
-
-
-
-
-
-
-
